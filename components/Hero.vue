e6e4d01bccd7884d1b5df5bf00db3ca818adbf56
Marlos Pomin <marlospomin@gmail.com>
2019-07-21 15:14:10 -0300

    Update hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,7 +2,9 @@
   <main class="hero is-fullheight">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <h1 class="title is-1">Coming Soon!</h1>
+        <h1 class="title is-1">
+          Coming Soon!
+        </h1>
       </div>
     </div>
   </main>
